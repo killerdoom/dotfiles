669bebfa7a956b0555a01c662fafd55f6bdc3c9f
killerdoom <bettinanari1207@gmail.com>
2026-02-27 00:59:45 -0300

    Update

--- volume.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-VOL=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int($2*100)}')
-
-if wpctl get-volume @DEFAULT_AUDIO_SINK@ | grep -q MUTED; then
-  echo " "
-else
-  echo "  ${VOL}% "
-fi
